f08ac2124fcde310d9607112b54b321529366018
name <gowluonade@gmail.com>
2023-10-27 17:27:14 -0500

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    manifest: {
+      // name: 'React App',
+      // short_name: 'React App',
+      // start_url: '/',
+      // display: 'standalone',
+      // background_color: '#ffffff',
+      // description: 'Web site created using create-react-app',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        }
+      ]
+    }
+  })]
 })
